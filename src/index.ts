f8962a8e504c1f9d2ccc714b2d5a9f74b8fb8095
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2022-11-22 09:26:57 +0100

    make more similar to example in F#

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
-import { fromEvent, map } from "rxjs";
-function getPos(evt: MouseEvent): number {
-  return evt.pageX;
-}
+import { fromEvent, interval, map } from "rxjs";
+//function getPos(evt: MouseEvent): number {
+//  return evt.pageX;
+//}
+//let getPositions = map(getPos);
+//let events = fromEvent<MouseEvent>(document, "mousemove")
+//let positions = getPositions(events)
 
 function add7(x: number): number {
   return x + 7;
 }
 let add7s = map(add7);
 
-let getPositions = map(getPos);
+const numbers = interval(1000);
 
-let events = fromEvent<MouseEvent>(document, "mousemove")
-let positions = getPositions(events)
-let posAnd7s = add7s(positions)
+let posAnd7s = add7s(numbers)
 
 posAnd7s.subscribe({
     next: (x: any) => logItem(x),
